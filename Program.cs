65a59eb46ee58acb8eb48a63e95f3a1ccc43947f
Kemilly Ferreira <73208400+KemillyFerreira@users.noreply.github.com>
2020-12-18 00:48:57 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Condicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // aqui vai o código do programa
+            // C# é uma linguagem case sensitive
+
+            // declarar as variaveis
+            float nota1, nota2, nota3, media;
+            string nomeCompleto;
+
+            // entrada de dados
+            Console.WriteLine("Digite o nome do aluno");
+            nomeCompleto = Console.ReadLine();
+            Console.Write("Digite a primeira nota: ");
+            nota1 = float.Parse (Console.ReadLine());
+
+            Console.Write("Digite o segundo valor: ");
+            nota2 = float.Parse(Console.ReadLine());
+            
+            Console.Write("Digite a terceira nota: ");
+            nota3 = float.Parse(Console.ReadLine());    
+
+            // Processamento
+            media = (nota1+nota2+nota3)/3;
+
+            // Exibir o resultado
+            if(media>5){
+                // valor verdadeiro
+                Console.WriteLine("O aluno "+nomeCompleto+" está aprovado com a média de: "+media);
+            }else{
+                // Valor falso
+                Console.WriteLine("O aluno está reprovado com a media de:"+media);                
+            }
+         }
+    }
+}
